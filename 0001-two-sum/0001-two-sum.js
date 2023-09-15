579ab454dd345d8c69691f73d12820e9d0050120
Can Göloğlu <can.gologlu@gmail.com>
2023-09-15 13:44:01 -0400

    Time: 63 ms (64.79%), Space: 44.2 MB (13.18%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -5,17 +5,17 @@
  */
 var twoSum = function (nums, target) {
     let sorted = nums.map((el, idx) => ({ el, idx }));
-    sorted = sorted.sort((a,b)=>a.el -b.el);
+    sorted = sorted.sort((a, b) => a.el - b.el);
     let smIdx = 0;
     let lgIdx = sorted.length - 1;
-    while(smIdx<lgIdx){
-    let smallest = sorted[smIdx].el, largest = sorted[lgIdx].el;
-    if(smallest+largest === target){
-      return [sorted[smIdx].idx,sorted[lgIdx].idx];
-    }else if(smallest+largest > target){
-      lgIdx --;
-    }else{
-      smIdx++;
+    while (smIdx < lgIdx) {
+        let smallest = sorted[smIdx].el, largest = sorted[lgIdx].el;
+        if (smallest + largest === target) {
+            return [sorted[smIdx].idx, sorted[lgIdx].idx];
+        } else if (smallest + largest > target) {
+            lgIdx--;
+        } else {
+            smIdx++;
+        }
     }
-  }
 };
