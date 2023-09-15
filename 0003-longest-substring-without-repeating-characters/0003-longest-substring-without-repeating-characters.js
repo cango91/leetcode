ea4244b65a0c69b6d0fc7c62b7e972ac3896fa81
Can Göloğlu <can.gologlu@gmail.com>
2023-09-15 14:14:31 -0400

    Time: 1224 ms (5%), Space: 50.4 MB (12.87%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -3,19 +3,18 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function (s) {
-    const constructSubstring = (start, string) => {
-        let substr = "";
-        for (let i = start; i < string.length; i++) {
-            if (substr.includes(string[i])) break;
-            substr += string[i];
-        }
-        return substr;
-    }
     let longest = 0;
     const candidates = [];
     for (let i = 0; i < s.length; i++) {
         if (s.length - i < longest) break;
-        candidates.push(constructSubstring(i,s).length);
+        candidates.push((() => {
+            let substr = "";
+            for (let j = i; j < s.length; j++) {
+                if (substr.includes(s[j])) break;
+                substr += s[j];
+            }
+            return substr.length;
+        })());
         longest = Math.max(...candidates);
     }
     return longest;
