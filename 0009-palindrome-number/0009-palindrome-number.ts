f8982f4026fcc055bce3ed75663c218a13598f58
Can Göloğlu <can.gologlu@gmail.com>
2023-10-05 15:17:49 -0400

    Time: 146 ms (53.52%), Space: 52.2 MB (23.97%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.ts
@@ -0,0 +1,11 @@
+function isPalindrome(x: number): boolean {
+    if(x<0) return false;
+    const s = String(x);
+    let l=0, h=s.length-1;
+    while(l<=h){
+        if(s.charCodeAt(l)!==s.charCodeAt(h)) return false;
+        l++;
+        h--;
+    }
+    return true;
+};
