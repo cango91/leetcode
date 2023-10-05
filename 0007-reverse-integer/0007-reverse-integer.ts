0068a7cfb1244027bd7770f47b883ff60b7cf8be
Can Göloğlu <can.gologlu@gmail.com>
2023-10-05 11:55:05 -0400

    Time: 61 ms (77.81%), Space: 44.8 MB (60.58%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.ts
+++ 0007-reverse-integer/0007-reverse-integer.ts
@@ -1,12 +1,13 @@
 function reverse(x: number): number {
-    const MAX_INT = 2**31 -1;
-    const MIN_INT = -1 * 2**31;
-    let result = 0;
-    while (x!==0){
-        let pop = x % 10;
-        x = (x-pop) / 10;
-        if(result<(MIN_INT-pop)/10 || result > (MAX_INT-pop)/10) return 0;
-        result = result * 10 + pop;
-    }
-    return result;
+    let strRet : string;
+    let strNum: string;
+    let neg = x<0;
+    if(neg) strNum=String(x).substring(1);
+    else
+    strNum=String(x);
+    strRet = strNum.split("").reverse().join("");
+    let ret = Number(strRet);
+    if(neg) ret *= -1;
+    if(ret >= -1 * 2**31 && ret<= 2**31 -1) return ret;
+    return 0;
 };
