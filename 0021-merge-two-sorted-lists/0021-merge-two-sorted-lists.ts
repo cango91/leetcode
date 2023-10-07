e1e8ad1bd20b9d60b7e095ff25188a6f91b891f1
Can Göloğlu <can.gologlu@gmail.com>
2023-10-07 14:26:34 -0400

    Time: 67 ms (37.09%), Space: 45 MB (44.36%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.ts
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.ts
@@ -13,13 +13,19 @@
 function mergeTwoLists(list1: ListNode | null, list2: ListNode | null): ListNode | null {
     if (!list1) return list2;
     if (!list2) return list1;
-    if (list2.val < list1.val) {
-        [list2, list1] = [list1, list2];
+
+    let head: ListNode | null;
+    let ptr: ListNode | null;
+
+    if (list1.val < list2.val) {
+        head = list1;
+        list1 = list1.next;
+    } else {
+        head = list2;
+        list2 = list2.next;
     }
 
-    let head = list1;
-    let ptr = head;
-    list1 = list1.next;
+    ptr = head;
     while (list1 && list2) {
         if (list1.val < list2.val) {
             ptr.next = list1;
