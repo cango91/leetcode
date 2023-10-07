dcf6df24b7949757396280bb4ba7befcd7dbe9eb
Can Göloğlu <can.gologlu@gmail.com>
2023-10-07 14:39:20 -0400

    Time: 90 ms (97.01%), Space: 46.5 MB (86.47%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function (s) {
+    const mappings = {
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000,
+    }
+    let result = 0;
+    let next = "";
+    for (let i = 0; i < s.length; i++) {
+        let val = mappings[s[i]];
+        if (i + 1 < s.length && mappings[s[i]] < mappings[s[i + 1]]) {
+            val *= -1;
+        }
+        result += val;
+    }
+    return result;
+};
