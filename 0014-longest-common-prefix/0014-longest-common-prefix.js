f21d664f8ad6c83d6a6e03db893b4b16d5047af8
Can Göloğlu <can.gologlu@gmail.com>
2023-10-11 10:55:50 -0400

    Time: 59 ms (34.44%), Space: 42.4 MB (55.31%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    if(!strs.length || strs.some(str=>str==="")) return "";
+    if(strs.length===1) return strs[0];
+    strs.sort((a,b)=> a.length - b.length);
+    let exit = false;
+    let idx = 0;
+    let ret = "";
+    while(!exit && idx<strs[0].length){
+        const char = strs[0][idx];
+        for(let i = 1; i<strs.length; i++){
+            if(strs[i][idx]!==char) exit = true;
+        }
+        if(!exit) ret += char;
+        idx++;
+    }
+    return ret;
+};
