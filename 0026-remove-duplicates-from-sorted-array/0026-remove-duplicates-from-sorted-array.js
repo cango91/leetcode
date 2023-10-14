443212081b52d7113a301d5de9978ab7cc232768
Can Göloğlu <can.gologlu@gmail.com>
2023-10-14 14:22:08 -0400

    Time: 68 ms (42.19%), Space: 44.4 MB (66.54%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -3,9 +3,15 @@
  * @return {number}
  */
 var removeDuplicates = function (nums) {
-    const sorted = [... new Set(nums)];
-    for(let i = 0; i<sorted.length; i++){
-        nums[i] = sorted[i];
+    let cache = null;
+    let ptr1 = 0, ptr2 = 0;
+    while (ptr1 < nums.length && ptr2 < nums.length) {
+        if (nums[ptr2] !== cache) {
+            cache = nums[ptr2];
+            nums[ptr1] = nums[ptr2];
+            ptr1++;
+        }
+        ptr2++;
     }
-    return sorted.length;
+    return ptr1;
 };
