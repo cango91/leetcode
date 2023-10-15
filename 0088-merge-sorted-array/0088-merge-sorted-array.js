6387e6f5146589a79b3c1fc34e55536c50a69a47
Can Göloğlu <can.gologlu@gmail.com>
2023-10-15 11:26:28 -0400

    Time: 53 ms (51.43%), Space: 42.3 MB (28.09%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.js
+++ 0088-merge-sorted-array/0088-merge-sorted-array.js
@@ -6,7 +6,24 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 var merge = function(nums1, m, nums2, n) {
-    nums1.splice(m,n);
-    nums2.forEach(n=>nums1.push(n))
-    nums1.sort((a,b)=>a-b);
+    if(!nums2.length || n===0) return;
+    const cache = [...nums1];
+    let j=0, k=0;
+    for(let i=0;i<m+n;i++){
+        if(j<m && k<n){
+            if(cache[j]<nums2[k]){
+                nums1[i] = cache[j];
+                j++;
+            }else{
+                nums1[i] = nums2[k];
+                k++;
+            }
+        }else if(k<n){
+            nums1[i] = nums2[k];
+            k++;
+        }else if(j<m){
+            nums1[i] = cache[j];
+            j++;
+        }
+    }
 };
